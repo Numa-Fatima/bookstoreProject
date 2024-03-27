bba631cc9b87fd4c25ba84ca212ed9577c0c043e
Numafa <numafatima7@gmail.com>
2024-03-27 16:18:06 +0300

    created auth.js files for controllers and routes and tested them successfully, still have to clean my code

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const user_route = require('./routes/bookstore')
+const auth_route = require('./routes/authRoutes'); 
+
+
 const app = express()
 const PORT = 3000
 
@@ -13,4 +16,6 @@ mongoose.connect('mongodb://localhost:27017/test').then(()=>{
   
 app.use(express.json())
 app.use('/bookstore',user_route)
+app.use('/auth', auth_route);
+
 
--- routes/bookstore.js
+++ routes/bookstore.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/bookstore')
 router.get('/',userController.getAllUsers)
 router.get('/:username',userController.getUserByUsername )
-router.post('/login', userController.loginUser);
-router.post('/',userController.createUser )
+//router.post('/login', userController.loginUser);
+//router.post('/',userController.createUser )
 router.put('/:email',userController.updateUserByEmail)
 
 module.exports = router
